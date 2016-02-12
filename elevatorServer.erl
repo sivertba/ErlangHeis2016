717c5440b34a1a7a1d1de7b28e871c6b892ac294
Sivert Bakken <sivert.bakken@gmail.com>
2016-02-12 22:53:04 +0100

    Prøver å finne ut hvordan ta ordre som en server, sliter

---
+++ elevatorServer.erl
@@ -0,0 +1,24 @@
+-module (elevatorServer).
+-behaviour (gen_server).
+
+%	Module APIs
+%-export ([function/arity]).
+
+%	gen_server callbacks
+-export ([	init/1, 
+			terminate/2,
+			handle_info/2,
+			handle_cast/2,
+			handle_call/3,
+			code_change/3]).
+
+
+
+
+%	gen_server functions
+init()
+terminate()
+handle_info()
+handle_cast()
+handle_call()
+code_change()
