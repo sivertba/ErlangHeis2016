55713d1594895256ca730aac81eaa5acdca5c09e
Sivert Bakken <sivert.bakken@gmail.com>
2016-02-17 18:42:55 +0100

    Fikk ikke til trenger hjelp

--- oving/Ex6/ex6.erl
+++ oving/Ex6/ex6.erl
@@ -1,4 +1,23 @@
 -module (ex6).
 -compile(export_all).
 
-%fml
+%Counter
+
+start() ->
+    register(backup,spawn(backup())).
+
+counter1() ->
+	counter1(0).
+counter1(N) ->
+    Backup ! {ok, N},
+	io:format("~p~n",[N]),
+	timer:sleep(500),
+	counter1(N+1).
+
+backup() ->
+    receive 
+        {ok, N} ->
+            % ???
+    after 5000 -> 
+        ok
+    end.
