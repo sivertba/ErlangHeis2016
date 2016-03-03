d24452c6829ada248285f4274921b5e596390a8c
Sivert Bakken <sivertba@users.noreply.github.com>
2016-03-03 16:15:47 +0100

    Connection-modulen lagt til
    Halvveis ferdig kinda

--- test.erl
+++ test.erl
@@ -1,14 +1,48 @@
 -module (test).
 -compile(export_all).
 
-%belongs to counter
--define (TIME_TO_NEXT, 100).
+init() ->
+	
+	Hosts = net_adm:host_file(),
+	Mum = self(),
 
-%can say something about time between orders
-counter(N) -> 
+	lists:foreach(fun(Elem) -> spawn(?MODULE,getNodes,[Elem,Mum]) end, Hosts),
+	
+	NodeListPID = spawn_monitor(?MODULE,list_control,[[]]),
+
+
+	 = list_builder([], NodeListPID).
+
+
+getNodes(Host, Listener) ->
+	timer:exit_after(3000, time_exceeded),
+	Message = net_adm:names(Host),
+	Listener ! {new_list_item, self(), Message}.
+
+
+tup(Tupel) ->
+	{Name,_Port} = Tupel,
+	net_adm:ping(Name).
+
+
+list_control(List) ->
 	receive
-		{status, Receiver} -> Receiver ! {N},counter(N);
-		{reset, Receiver} -> Receiver ! {ok}, counter(0)
-	after ?TIME_TO_NEXT ->
-		counter(N+1)
+		{add, ToAdd} ->
+			list_control(List ++ ToAdd);
+		{send, Pid} ->
+			Pid ! {List}
 	end.
+
+
+list_builder(NodeList, []) ->
+	NodeList;
+list_builder(NodeList, PidList) ->
+	receive
+		{new_list_item, Pid, {ok, Item}} ->
+			list_builder([Item|List], lists:delete(Pid, PidList));
+		{new_list_item, Pid, {error, _Reason}} ->
+			list_builder(List, lists:delete(Pid, PidList));
+		{'DOWN', _Ref, process, PID, _Reason} ->
+			list_builder(List, lists:delete(Pid, PidList))
+	end.
+	
