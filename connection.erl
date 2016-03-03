96f88209dcd4ba69e1111c3b4e3ede8b3565d0c2
mariannawk <mariannawk@hotmail.com>
2016-03-03 23:02:59 +0100

    Endret fremgangsmåte, burde funke
    Endret til å samle opp alle responsive hosts og så la world_list ta seg av det.

--- connection.erl
+++ connection.erl
@@ -8,34 +8,33 @@ init() ->
 	PidList = lists:map(fun(Elem) -> spawn_monitor(?MODULE, getNodes, [Elem, SelfPid]) end, Hosts),
 	% spawn_monitor returns {Pid, Ref}!!! Shit ensues
 
-	NodeList = list_builder([], PidList),
-	PingResults = lists:map(fun connection:unpackPing/1, NodeList),
-
-	% klarer aa pinge, faar negative svar ??	
-	erlang:display(PingResults),
+	ActiveHosts = list_builder([], PidList),
+	net_adm:world_list(ActiveHosts),
 	nodes().
-
+	
+	%PingResults = lists:map(fun connection:unpackPing/1, NodeList),
+	
 
 getNodes(Host, Listener) ->
-	timer:exit_after(3000, time_exceeded),
+	timer:exit_after(2000, time_exceeded),
 	Message = net_adm:names(Host),
-	Listener ! {new_list_item, self(), Message}.
+	Listener ! {new_list_item, self(), Host, Message}. %knotete
 
-
+% unodvendig naa
 unpackPing({Name, _Port}) ->
 	net_adm:ping(list_to_atom(Name)).
 
 
-list_builder(NodeList, []) ->
-	NodeList;
+list_builder(HostList, []) ->
+	HostList;
 list_builder(NodeList, PidList) ->
 	receive
-		{new_list_item, Pid, {ok, Item}} ->
-			list_builder(Item++NodeList, lists:keydelete(Pid, 1, PidList));
-		{new_list_item, Pid, {error, _Reason}} ->
-			list_builder(NodeList, lists:keydelete(Pid, 1, PidList));
+		{new_list_item, Pid, Host, {ok, _Nodes}} ->
+			list_builder([Host | HostList], lists:keydelete(Pid, 1, PidList));
+		{new_list_item, Pid, _Host, {error, _Reason}} ->
+			list_builder(HostList, lists:keydelete(Pid, 1, PidList));
 		{'DOWN', _Ref, process, Pid, _Reason} ->
-			list_builder(NodeList, lists:keydelete(Pid, 1, PidList))
+			list_builder(HostList, lists:keydelete(Pid, 1, PidList))
 	end.
 
 	% lager predikat for pid-dropping
