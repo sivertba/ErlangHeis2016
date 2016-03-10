1167863ebd2d0b0f95fb029fe2e0dc55690d2718
Sivert Bakken <sivert.bakken@gmail.com>
2016-03-10 19:39:08 +0100

    audyisod

--- order_handler.erl
+++ order_handler.erl
@@ -8,18 +8,24 @@
 			get_next_order/3]).
 
 -define(QUEUE_PID, queue).
-%-define(DETS_TABLE_NAME, "orders").
+-define(DETS_TABLE_NAME, "ordersETS").
 -define(ORDER_EXECUTION_DEADLINE, 10000). %10 seconds
 
--record (order, {floor,direction, timestamp = erlang:timestamp(), node = node()}).
+-record (order, {floor,direction,timestamp = erlang:timestamp()}).
 
 %Må endres
 start() ->
-	register(?QUEUE_PID,spawn(?MODULE,order_bank,[])).
+	%Burde gjøres i "main"
+	register(?QUEUE_PID,spawn(?MODULE,order_bank,[])),
+	
+	dets:open_file(?DETS_TABLE_NAME, {filename, ?DETS_TABLE_NAME,acess,read}),
+	% ???
+
+	ok.
 
 add_order(Floor,Direction) ->
 	add_order(#order{floor = Floor, direction = Direction}).
-add_order(Order) when Order#order.direction =:= command -> 
+add_order(Order) when Order#order.direction == command -> 
 	?QUEUE_PID ! {add, Order};
 add_order(Order) ->
 	?QUEUE_PID ! {add, Order},
@@ -31,9 +37,10 @@ get_orders() ->
 		L -> L
 	end.
 
+%Denne er helt feil nå
 get_next_order(Last_floor, Direction,Manager) ->
 	L = get_orders(),
-	if	L =:= [] -> 
+	if	L == [] -> 
 			?MODULE:get_next_order(Last_floor, Direction,Manager);
 		L =/= [] -> 
 		 	Order = order_list_filter(Last_floor,Direction,L),
@@ -46,73 +53,89 @@ remove_order(Order) ->
 	?QUEUE_PID ! {remove, Order},
 	send_to_queue_on_nodes({remove, Order}).
 
-retract_order(Order) when Order#order.direction =:= command -> 
-	?QUEUE_PID ! {retract, Order};
-retract_order(Order) ->
-	?QUEUE_PID ! {retract, Order},
-	send_to_queue_on_nodes({retract, Order}).
-
 order_monitor(Order,Manager) ->
 	receive
 		{order_handeld} -> 
 			remove_order(Order);
 		{order_aborted} -> 
-			retract_order(Order)
+			add_order(Order)
 	after ?ORDER_EXECUTION_DEADLINE -> 
 		Manager ! timeout,
-		retract_order(Order)
+		add_order(Order)
 	end.
 
-%Burde skrives om til å lagre på disk (dets)
-order_bank() ->
-	order_bank([]).
+%Burde skrives om til å lagre på disk (dets) også
 order_bank(L) ->
+	%DETS HER
 	receive
 		{get_orders, Pid} ->
 			Pid ! L,
-			?MODULE:order_bank(remove_duplicates(L));
+			?MODULE:order_bank(L);
 		{add, Order} -> 
-			NewL = lists:append(L,[Order]),
-			?MODULE:order_bank(remove_duplicates(NewL));
+			?MODULE:order_bank(remove_duplicates(Order,L));
 		{remove, Order} -> 
-			?MODULE:order_bank(lists:delete(Order, L));
-		{retract, Order} ->
-			NewL = lists:append([Order],L),
-			?MODULE:order_bank(remove_duplicates(NewL))
+			?MODULE:order_bank(lists:delete(Order, L))
 	end.
 
+remove_duplicates(Order,L) ->
+	Bool = lists:any(fun(E) -> order_match(E,Order) end, L),
+	if
+		 Bool->
+			L;
+		true -> 
+			L ++ Order	
+	end.	
 
-remove_duplicates(L) ->
-	%Må se bort i fra node og tid
-	L.	
+order_match(A,B) -> 
+	if
+		(A#order.floor == B#order.floor) andalso (A#order.direction == B#order.direction) ->
+			true;
+		(A#order.floor /= B#order.floor) or (A#order.direction /= B#order.direction) ->
+			false
+	end.
+
+floor_match(A,B) -> 
+	if
+		(A#order.floor /= B#order.floor) -> true;
+		(A#order.floor /= B#order.floor) ->	false
+	end.
+
+timestamp_compare(A,B) -> 
+	if 
+		A#order.timestamp =< B#order.timestamp -> true;
+		A#order.timestamp > B#order.timestamp -> false
+	end.
+
+floor_compare(A,B) ->
+	if
+		A#order.floor =< B#order.floor -> true;
+		A#order.floor > B#order.floor -> false
+	end.
+
+order_is_in_list(O,List) ->
+	lists:any(fun(E) -> order_match(E,O) end ,List).
+
 order_list_filter(Last_floor,Direction,L) ->
 	PossibleOrders = lists:filter(fun(Elem) -> on_path(Elem,Last_floor,Direction) end, L),
-	SortedOrders = 	ordsets:from_list(PossibleOrders),
-	if
-		Direction =:= up ->
-			lists:nth(1,SortedOrders);
-		Direction =:= down ->
-			lists:last(SortedOrders);
-		true -> %else
-			lists:nth(1,SortedOrders)
-	end.
+	SortedOrders = lists:sort(fun(A,B) -> timestamp_compare(A,B) end, PossibleOrders),
+	lists:nth(1,SortedOrders).
 
 on_path(#order{floor=Floor,direction=Dir},Last_floor, Direction) ->
-	if 	((Dir =:= up) or (Dir =:= command)) and (Direction =:= up) ->
+	if 	((Dir == up) or (Dir == command)) and (Direction == up) ->
 			if
 				Last_floor > Floor -> 
 					false;
 				Last_floor =< Floor -> 
 					true
 			end;
-		((Dir =:= down) or (Dir =:= command)) and (Direction =:= down) ->
+		((Dir == down) or (Dir == command)) and (Direction == down) ->
 			if
 				Last_floor < Floor -> 
 					false;
 				Last_floor >= Floor-> 
 					true
 			end;
-		Direction =:= command -> 
+		Direction == command -> 
 			true;
 		true -> 
 			false
@@ -120,3 +143,12 @@ on_path(#order{floor=Floor,direction=Dir},Last_floor, Direction) ->
 
 send_to_queue_on_nodes(Msg) ->
 	lists:foreach(fun(Node) -> {?QUEUE_PID, Node} ! Msg end,nodes()).
+
+%Må tenke bedre ...
+orderUpdate(L) ->
+	send_to_queue_on_nodes({get_orders,self()}),
+	receive
+		Orders -> ordsets:intersection(L,Orders)
+			lists:append(L,Orders)
+		after 50 -> ordsets:to_listj(L)
+	end.
