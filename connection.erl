a5ae7fe5909fd0808b740e95dd950441b1ae30f7
ingvildforseth <ingvild-f@hotmail.com>
2016-04-07 17:03:18 +0200

    Hybrid mellom gammel og ny, skal funke

--- connection.erl
+++ connection.erl
@@ -3,24 +3,33 @@
 
 init() ->
 	{_ok, [LongIPtuple | _Tail]} = inet:getif(),
-	NodeName = list_to_atom("heis@"++tuple_to_stringIP(element(1, LongIPtuple))),
+	NodeName = list_to_atom("heis@"++format_IP(element(1, LongIPtuple))),
 	net_kernel:start([NodeName, longnames, 500]),
 	erlang:set_cookie(node(), 'kake'),
 
-
 	Hosts = net_adm:host_file(),
-	SelfPid = self(), % shit losning, unnskyld Kjetil & Erlang-guder
-
-	connect(Hosts, SelfPid).
+	erlang:display(Hosts),
+	connect(Hosts).
 	
 	%PingResults = lists:map(fun connection:unpackPing/1, NodeList),
 
-connect(Hosts, SelfPid) ->
-	PidList = lists:map(fun(Elem) -> spawn_monitor(?MODULE, getNodes, [Elem, SelfPid]) end, Hosts),
-	% spawn_monitor returns {Pid, Ref}
-	Nodes = nodelist_builder([], PidList),
-	lists:foreach(fun(Node) -> net_adm:ping(Node) end, Nodes),
-	connect(Hosts, SelfPid).
+
+connect(Hosts) ->
+	PidList = lists:map(fun(Elem) -> spawn_monitor(?MODULE, getNodes, [Elem, self()]) end, Hosts),
+	ResponsiveHosts = hostlist_builder([], PidList),
+	net_adm:world_list(ResponsiveHosts),
+	%lists:foreach(fun(Host) -> net_adm:ping(list_to_atom("heis@"++atom_to_list(Host))) end, ResponsiveHosts),
+	connect(Hosts).
+
+
+
+
+%old_connect(Hosts, SelfPid) ->
+%	PidList = lists:map(fun(Elem) -> spawn_monitor(?MODULE, getNodes, [Elem, SelfPid]) end, Hosts),
+%	% spawn_monitor returns {Pid, Ref}
+%	Nodes = nodelist_builder([], PidList),
+%	lists:foreach(fun(Node) -> net_adm:ping(Node) end, Nodes),
+%	old_connect(Hosts, SelfPid).
 
 
 getNodes(Host, Listener) ->
@@ -32,22 +41,35 @@ getNodes(Host, Listener) ->
 %unpackPing({Name, _Port}) ->
 	%net_adm:ping(list_to_atom(Name)).
 
-
-nodelist_builder(NodeList, []) ->
-	NodeList;
-nodelist_builder(NodeList, PidList) ->
+hostlist_builder(HostList, []) ->
+	HostList;
+hostlist_builder(HostList, PidList) ->
 	receive
-		{new_list_item, Pid, Host, {ok, RawNodes}} ->
-			Nodes = lists:map(fun({Name, _Port}) -> list_to_atom(Name++"@"++atom_to_list(Host)) end, RawNodes),
-			nodelist_builder(Nodes++NodeList, lists:keydelete(Pid, 1, PidList));
+		{new_list_item, Pid, Host, {ok, _Nodes}} ->
+			hostlist_builder([Host | HostList], lists:keydelete(Pid, 1, PidList));
 		{new_list_item, Pid, _Host, {error, _Reason}} ->
-			nodelist_builder(NodeList, lists:keydelete(Pid, 1, PidList));
+			hostlist_builder(HostList, lists:keydelete(Pid, 1, PidList));
 		{'DOWN', _Ref, process, Pid, _Reason} ->
-			nodelist_builder(NodeList, lists:keydelete(Pid, 1, PidList))
+			hostlist_builder(HostList, lists:keydelete(Pid, 1, PidList))
 	end.
 
 
-tuple_to_stringIP(IPtuple) ->
+
+%nodelist_builder(NodeList, []) ->
+%	NodeList;
+%nodelist_builder(NodeList, PidList) ->
+%	receive
+%		{new_list_item, Pid, Host, {ok, RawNodes}} ->
+%			Nodes = lists:map(fun({Name, _Port}) -> list_to_atom(Name++"@"++atom_to_list(Host)) end, RawNodes),
+%			nodelist_builder(Nodes++NodeList, lists:keydelete(Pid, 1, PidList));
+%		{new_list_item, Pid, _Host, {error, _Reason}} ->
+%			nodelist_builder(NodeList, lists:keydelete(Pid, 1, PidList));
+%		{'DOWN', _Ref, process, Pid, _Reason} ->
+%			nodelist_builder(NodeList, lists:keydelete(Pid, 1, PidList))
+%	end.
+
+
+format_IP(IPtuple) ->
 	[_Head | IPlist] = lists:flatmap(fun(X) -> ['.', X] end, tuple_to_list(IPtuple)),
 	lists:concat(IPlist).
 
