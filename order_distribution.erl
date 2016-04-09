42ba36ede2757f6f134c87a905121fee00176112
Martin Ø. Christensen <martin.o.christensen@gmail.com>
2016-04-09 16:41:34 +0200

    Fikset sånn at den håndterer riktig

--- order_distribution.erl
+++ order_distribution.erl
@@ -2,58 +2,53 @@
 % og forovrig full av syntaksfeil
 
 -module(order_distribution).
--include("orders.hrl").
+-include("records_and_macros.hrl").
 -compile(export_all). %not really????
 
 % state supervisor returnerer {{State, LastFloor, LastDir}, node()}
-% State = idle/moving/stuck
+% State = stationary/moving/stuck
 
 
 cost_function({ElevState, ElevFloor, ElevLastDir}, OrderFloor) ->
   Difference = OrderFloor - ElevFloor, %if negative, order is below
-  erlang:abs(Difference) + movement_penalty(ElevLastDir, Difference) + turn_penalty(ElevFloor, ElevLastDir, Difference).
+  case ElevState of
+  	stuck -> 5;
+  	_ ->
+  		Difference = OrderFloor - ElevFloor,
+  		erlang:abs(Difference) + movement_penalty(ElevState, ElevLastDir, Difference) + turn_penalty(ElevState, ElevFloor, ElevLastDir, Difference)
+  end.
   
 % hjelpefunksjon for kostfunksjon (burde nøstes som lambda men det ble knot)
-movement_penalty(idle, _) -> 0;
-movement_penalty(_, 0) -> 1.5; %vi er pa vei vekk
-movement_penalty(Dir, FloorDif) when (Dir == up andalso FloorDif > 0) orelse (Dir == down andalso FloorDif < 0) -> -0.5;
-movement_penalty(Dir, FloorDif) when (Dir == up andalso FloorDif < 0) orelse (Dir == down andalso FloorDif > 0) -> 1.5.
+movement_penalty(stationary, _Dir, _Dif) -> 0;
+movement_penalty(_State, _Dir, 0) -> 1.5; %vi er pa vei vekk
+movement_penalty(_State, Dir, FloorDif) when (Dir == up andalso FloorDif > 0) orelse (Dir == down andalso FloorDif < 0) -> -0.5;
+movement_penalty(_State, Dir, FloorDif) when (Dir == up andalso FloorDif < 0) orelse (Dir == down andalso FloorDif > 0) -> 1.5.
 
-turn_penalty(_ElevFloor, _Dir, 0) -> 0;
-turn_penalty(ElevFloor, _Dir, _Dif) when ElevFloor == 0; ElevFloor == 3 -> 0;
-turn_penalty(_ElevFloor, Dir, Dif) when (Dir == up andalso Dif > 0) orelse (Dir == down andalso Dif < 0) -> 0;
-turn_penalty(_ElevFloor, Dir, Dif) when (Dir == up andalso Dif < 0) orelse (Dir == down andalso Dif > 0) -> 0.75.
+turn_penalty(stationary, ElevFloor, _Dir, _Dif) when ElevFloor == 0; ElevFloor == (?NUMBER_OF_FLOORS-1) -> 0;
+turn_penalty(moving, ElevFloor, Dir, _Dif) when (ElevFloor == 1 andalso Dir == down); (ElevFloor == (?NUMBER_OF_FLOORS-2) andalso Dir == up) -> 0;
+turn_penalty(stationary, _ElevFloor, Dir, Dif) when (Dir == up andalso Dif >= 0) orelse (Dir == down andalso Dif =< 0) -> 0;
+turn_penalty(moving, _ElevFloor, Dir, Dif) when (Dir == up andalso Dif > 0) orelse (Dir == down andalso Dif < 0) -> 0;
+turn_penalty(_ElevState, _ElevFloor, _Dir, _Dif) -> 0.75.
 
 % forklaring av verdier:
 % om heisen er på vei mot ordren, er det bedre enn om den stod stille der den nettopp stod, men verre enn om den stod stille der den snart er
 % om heisen er på vei vekk, er det +1 fordi den vil være en etasje lenger vekk når den kan handtere ordren, +0.5 fordi det tar tid til den er klar
 
 % manager-oppførsel:
-direction_picker() ->
-  Elevators = elevator:get_states(),
-  % hent ordreliste, med eldste først
-  direction_picker(Elevators, Orders);
-direction_picker(_Elevators, []) ->
-  timer:sleep(200),
-  direction_picker();
-direction_picker(Elevators, Orders) ->
-  [Oldest|Rest] = Orders,
-  {BestCost, BestNode} = min(lists:map(fun({{State, Floor, Dir}, Node}) -> {cost_function({State, Floor, Dir} Oldest#order.floor), Node} end, Elevators))
+action_select(_Elevators, []) -> wait;
+action_select(Elevators, Orders) ->
+  [Oldest | Rest] = Orders,
+  {_BestCost, BestNode, BestFloor} = lists:min(lists:map(fun({{State, Floor, Dir}, Node}) -> {cost_function({State, Floor, Dir}, Oldest#order.floor), Node, Floor} end, Elevators)),
   case BestNode == node() of
     true ->
-      case BestCost of
-          0 -> open_doors;
-          _ ->
-            if 
-            % OBS! SelfFloor er udefinert RN
-            Oldest#order.floor < SelfFloor -> down;
-            Oldest#order.floor > SelfFloor -> up
-          end
-      end;
+    	if
+    		Oldest#order.floor == BestFloor -> doors_open; 
+    		Oldest#order.floor < BestFloor -> down;
+    		Oldest#order.floor > BestFloor -> up
+    	end;
     false ->
-      direction_picker({{SelfState, SelfFloor, SelfDir}, SelfNode}, NeighbStates, Rest)
-  end
-end.
+      action_select(Elevators, Rest)
+  end.
   
   % prinsipp: se eldste ordre, er jeg narmeste heis? ja -> kjor, nei -> se etter nyere ordre
   % hvis ingen ordre, vent, prov igjen
