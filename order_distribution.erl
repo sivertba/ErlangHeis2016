7cbeb8e597541bfd6afce99294a98b312ccf019e
larserikjunge <ingvild-f@hotmail.com>
2016-04-14 16:21:39 +0200

    Fikset vekter og glitch

--- order_distribution.erl
+++ order_distribution.erl
@@ -1,49 +1,33 @@
 -module(order_distribution).
 -include("records_and_macros.hrl").
--compile(export_all). %not really????
-
-% state supervisor returnerer {{State, LastFloor, LastDir}, node()}
-% State = stationary/moving/stuck
-
+-compile(export_all).
 
 cost_function({ElevState, ElevFloor, ElevLastDir}, OrderFloor, OrderDir) ->
   case ElevState of
-  	stuck -> 10;
+  	stuck -> 3*?NUMBER_OF_FLOORS;
   	_ ->
       Difference = OrderFloor - ElevFloor, %if negative, order is below
-      Cost = erlang:abs(Difference) + movement_penalty(ElevState, ElevLastDir, Difference) + turn_penalty(ElevState, ElevFloor, ElevLastDir, OrderFloor) + order_dir_penalty(ElevLastDir, OrderDir),
-      %erlang:display(["Order ", OrderFloor, OrderDir, "Costs ", Cost, "From ", ElevFloor, ElevLastDir]),
-      Cost
+      erlang:abs(Difference) + movement_penalty(ElevState, ElevLastDir, Difference) + turn_penalty(ElevState, ElevFloor, ElevLastDir, OrderFloor) + order_dir_penalty(ElevLastDir, OrderFloor, OrderDir)
   end.
   
-% hjelpefunksjon for kostfunksjon (burde nøstes som lambda men det ble knot)
+% Cost modifier if traveling to or from order floor
 movement_penalty(stationary, _Dir, _Dif) -> 0;
-movement_penalty(_State, _Dir, 0) -> 1.5; %vi er pa vei vekk
+movement_penalty(_State, _Dir, 0) -> 1.5;
 movement_penalty(_State, Dir, FloorDif) when (Dir == up andalso FloorDif > 0) orelse (Dir == down andalso FloorDif < 0) -> -0.5;
 movement_penalty(_State, Dir, FloorDif) when (Dir == up andalso FloorDif < 0) orelse (Dir == down andalso FloorDif > 0) -> 1.5.
 
-%Penalty for getting there:
+% Penalty for changing direction of travel
 turn_penalty(stationary, ElevFloor, _ElevDir, _OrderFloor) when ElevFloor == 0 orelse ElevFloor == (?NUMBER_OF_FLOORS-1) -> 0;
 turn_penalty(moving, ElevFloor, ElevDir, _OrderFloor) when (ElevFloor == 1 andalso ElevDir == down); (ElevFloor == (?NUMBER_OF_FLOORS-2) andalso ElevDir == up) -> 0;
 turn_penalty(_ElevState, ElevFloor, ElevDir, OrderFloor) when (ElevDir == up andalso OrderFloor < ElevFloor) orelse (ElevDir == down andalso OrderFloor > ElevFloor) -> 0.75;
 turn_penalty(_ElevState, _ElevFloor, _ElevDir, _OrderFloor) -> 0.
 
-order_dir_penalty(ElevDir, OrderDir) when OrderDir /= command andalso ElevDir /= OrderDir -> 2.75;
-order_dir_penalty(_ElevDir, _OrderDir) -> 0.
+% Elevators are not allowed to handle orders in wrong direction if there are orders beyond, cost must agree
+order_dir_penalty(_ElevDir, OrderFloor, _OrderDir) when OrderFloor == 0 orelse OrderFloor == ?NUMBER_OF_FLOORS-1 -> 0;
+order_dir_penalty(ElevDir, _OrderFloor, OrderDir) when OrderDir /= command andalso ElevDir /= OrderDir -> ?NUMBER_OF_FLOORS-2+0.25;
+order_dir_penalty(_ElevDir, _OrderFloor, _OrderDir) -> 0.
 
-
-% Penalty for aiming at orders in wrong direction:
-%turn_penalty(_ElevState, _ElevFloor, _ElevDir, OrderFloor, _OrderDir) when OrderFloor == 0 orelse OrderFloor == (?NUMBER_OF_FLOORS-1) -> 0;
-%turn_penalty(_ElevState, _ElevFloor, ElevDir, _OrderFloor, OrderDir) when OrderDir /= command andalso ElevDir /= OrderDir-> 3;
-%turn_penalty(stationary, _ElevFloor, Dir, Dif) when (Dir == up andalso Dif > 0) orelse (Dir == down andalso Dif < 0) -> 0;
-%turn_penalty(moving, _ElevFloor, Dir, Dif) when (Dir == up andalso Dif > 0) orelse (Dir == down andalso Dif < 0) -> 0;
-%turn_penalty(_ElevState, _ElevFloor, _ElevDir, _OrderFloor, _OrderDir) -> 0.
-
-% forklaring av verdier:
-% om heisen er på vei mot ordren, er det bedre enn om den stod stille der den nettopp stod, men verre enn om den stod stille der den snart er
-% om heisen er på vei vekk, er det +1 fordi den vil være en etasje lenger vekk når den kan handtere ordren, +0.5 fordi det tar tid til den er klar
-
-% manager-oppførsel:
+% Elevators expects a list of tuples {{State, LastFloor, LastDir}, node()}
 action_select(_Elevators, []) -> wait;
 action_select(Elevators, Orders) ->
   {value, {LocalState, _Node}, OtherElevs} = lists:keytake(node(), 2, Elevators),
@@ -61,42 +45,13 @@ action_select(Elevators, Orders) ->
       end
   end.
 
-  %[Oldest | Rest] = Orders,
-  %{_BestCost, BestNode, BestFloor} 
-  %{_BestCost, BestNode, BestFloor} = lists:min(lists:map(fun({{State, Floor, Dir}, Node}) -> {cost_function({State, Floor, Dir}, Oldest#order.floor, Oldest#order.direction), Node, Floor} end, Elevators)),
-  %case BestNode == node() of
-  %  true ->
-  %  	if
-  %  		Oldest#order.floor == BestFloor -> open_doors; 
-  %  		Oldest#order.floor < BestFloor -> down;
-  %  		Oldest#order.floor > BestFloor -> up
-  %  	end;
-  %  false ->
-  %  	action_select(Elevators, Rest)
-  %end.
-  
-  % prinsipp: se eldste ordre, er jeg narmeste heis? ja -> kjor, nei -> se etter nyere ordre
-  % hvis ingen ordre, vent, prov igjen
-  % problemer: to heiser som er like narme vil begge kjore
-  % ved faerre enn 3 heiser resulterer det i ineffektiv oppforsel (i hvert fall ved 1 heis!)
-  % NEI! :DD pga blindekjoringsprinsippet :D hvis det er noe pa veien stopper vi der forst
-
-
-  find_next_order([], _OtherOrders) -> none;
-  find_next_order([{BestCost, BestOrder}| Rest], OtherOrders) ->
-    case lists:keyfind(BestOrder, 2, OtherOrders) of
-      false ->
-        erlang:display("Other elevator does not have order"),
-        erlang:display("Won order, cost:"),
-        erlang:display(BestOrder),
-        erlang:display(BestCost),
-        BestOrder;
-      {OtherCost, _Order, Node} when (OtherCost > BestCost) orelse (OtherCost == BestCost andalso Node > node()) ->
-        erlang:display("Won order, cost:"),
-        erlang:display(BestOrder),
-        erlang:display(BestCost),
-        BestOrder;
-      _ ->
-        erlang:display("Lost order, finding another"),
-        find_next_order(Rest, OtherOrders)
-    end.
+find_next_order([], _OtherOrders) -> none;
+find_next_order([{BestCost, BestOrder}| Rest], OtherOrders) ->
+  case lists:keyfind(BestOrder, 2, OtherOrders) of
+    false ->
+      BestOrder;
+    {OtherCost, _Order, Node} when (OtherCost > BestCost) orelse (OtherCost == BestCost andalso Node > node()) ->
+      BestOrder;
+    _ ->
+      find_next_order(Rest, OtherOrders)
+  end.
